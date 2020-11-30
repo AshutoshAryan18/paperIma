a934df809c46e21ea322729294d2c932ee6221da
AshutoshAryan18 <72258793+AshutoshAryan18@users.noreply.github.com>
2020-11-30 11:53:51 +0530

    Update box.js

--- box.js
+++ box.js
@@ -7,7 +7,7 @@ class Box {
         this.body=Bodies.rectangle(x,y,width,height,object);
         this.width=width;
         this.height=height;
-        this.image = loadImage("sprites/dustbingreen.png");
+        this.image = loadImage("dustbingreen.png");
         World.add(world,this.body);
     }
     display(){
